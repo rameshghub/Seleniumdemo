56cdec94702481b4a3a7fe2bb2c489b986f84801
Ramesh Adimulam <radimulam@virtusa.com>
2020-06-03 13:24:34 +0530

    newcomit

---
+++ eclipse-workspace/NewDemo/src/test/java/com/dxt/test1/mavendemo.java
@@ -0,0 +1,22 @@
+package com.dxt.test1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class mavendemo {
+
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a+b);
+	}
+		@Test
+		public void sub() {
+			System.out.println("SUB");
+			int a = 10;
+			int b = 20;
+			Assert.assertEquals(10, b-a);
+		}
+	}
